60806925dc9b4f67eeae15eb639d3831b3e90498
cjcanno <98831985+cjcanno@users.noreply.github.com>
2022-12-07 23:25:14 -0500

    Update ISS-API-Script.py

--- ISS-API-Script.py
+++ ISS-API-Script.py
@@ -10,15 +10,14 @@ req = http.request('GET', "http://api.open-notify.org/iss-now.json")
 
 data = json.loads(req.data.decode('utf-8')) #Takes the data from urllib3 and loads into json
 
-for key, value in data.items(): #goes over each KVP and prints
+for key, value in data.items(): # goes over each KVP and prints
     print(key, value, sep=':')
 
 webex_data = json.dumps(data)
 #######################Begin Webex Requirements##############
 
-ACCESS_TOKEN = 'N2E5YTFiNTctNzFkMC00ODRjLWEyNTctYmJlZGJhYjkzNDE4MTNhMjMwNTctNGY4_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
-roomID = 'Y2lzY29zcGFyazovL3VzL1JPT00vYWEyZGNjODAtNmE4Yi0xMWVkLWJiMjUtZDdhNTg1ZWE0NzM2'
-
+ACCESS_TOKEN = '' ## use webex token here
+roomID = '' # Insert webex room here
 
 def SetHeaders():
     accessToken_hdr = 'Bearer ' + ACCESS_TOKEN
